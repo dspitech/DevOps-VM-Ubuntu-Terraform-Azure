26fa9800e4c0badb6ac529b8d0732d2139d64a69
DSPI-TECH <94603413+dspitech@users.noreply.github.com>
2026-06-24 18:29:10 +0200

    fix : Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,13 +2,13 @@
 # Backend Terraform — Azure Storage Account
 # ============================================================
 # À exécuter AVANT terraform init :
-#   ./scripts/setup-backend.sh
+#   ./setup-backend.sh
 # ============================================================
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "OpenLab-TFState-RG"
-    storage_account_name = "openlabtfstate"   # doit être globalement unique
+    storage_account_name = "openlabtfstate"   
     container_name       = "tfstate"
     key                  = "openlab-vm.terraform.tfstate"
   }
